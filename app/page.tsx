4994f427631e2ff688fb96342fab117177fa2d15
Denys Kifor <kifor.denys@outlook.com>
2025-01-05 15:45:50 +0200

    Added header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <h1>
-      Home
-      <Button variant={"outline"}>Кошик</Button>
-    </h1>
-  );
+  return <h1></h1>;
 }
