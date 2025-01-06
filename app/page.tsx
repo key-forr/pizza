bfe98071fab8b6d0a2b33c60969578c953f0fa93
Denys Kifor <kifor.denys@outlook.com>
2025-01-06 14:13:46 +0200

    Added filters

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Title, TopBar } from "@/components/shared";
+import { Container, Title, TopBar, Filters } from "@/components/shared";
 
 export default function Home() {
   return (
@@ -7,6 +7,19 @@ export default function Home() {
         <Title text="Всі піци" size="lg" className="font-extrabold" />
       </Container>
       <TopBar />
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          {/* {Фільтрація} */}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          {/* {Список товарів} */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Список товарів</div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }
