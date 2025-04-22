7c082569050ab39010bc5048681838baa9f5ce15
key-forr <denyskifor@gmail.com>
2025-04-22 11:33:26 +0300

    The product component has been moved to a separate component so that there is no duplication of code for the modal window and for the separate product page.

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -1,9 +1,4 @@
-import {
-  Container,
-  PizzaImage,
-  Title,
-  GroupVariants,
-} from "@/components/shared";
+import { Container, ProductForm } from "@/components/shared";
 import { prisma } from "@/prisma/prisma-client";
 import { notFound } from "next/navigation";
 
@@ -28,45 +23,14 @@ export default async function ProductPage({
       items: true,
     },
   });
+
   if (!product) {
     return notFound();
   }
 
   return (
     <Container className="flex flex-col my-10">
-      <div className="flex flex-1">
-        <PizzaImage imageUrl={product.imageUrl} size={40} />
-        <div className="w-[490px] bg-[#F7F6F5] p-7">
-          <Title
-            text={product.name}
-            size="md"
-            className="font-extrabold mb-1"
-          />
-
-          <p className="text-gray-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, ipsa
-            deleniti.
-          </p>
-
-          <GroupVariants
-            value="2"
-            items={[
-              {
-                name: "Мала",
-                value: "1",
-              },
-              {
-                name: "Середня",
-                value: "2",
-              },
-              {
-                name: "Велика",
-                value: "3",
-              },
-            ]}
-          />
-        </div>
-      </div>
+      <ProductForm product={product} />
     </Container>
   );
 }
