4546e00bb9d3dd41e903569357feec31048bd656
key-forr <denyskifor@gmail.com>
2025-04-22 12:01:49 +0300

    Added information, i.e. the product becomes unavailable to the cart when deleted

--- components/shared/cart-drawer.tsx
+++ components/shared/cart-drawer.tsx
@@ -70,6 +70,7 @@ export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({
                       )
                     : ""
                 }
+                disabled={item.disabled}
                 name={item.name}
                 price={item.price}
                 quantity={item.quantity}
