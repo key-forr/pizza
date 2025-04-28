fd5d4eecf7c17a45202bbe40225e00d46b506fe4
key-forr <denyskifor@gmail.com>
2025-04-28 16:50:58 +0300

    fix bug with cart item, and cart without item

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -78,18 +78,18 @@ export async function POST(req: NextRequest) {
           quantity: findCartItem.quantity + 1,
         },
       });
-    }
-
-    await prisma.cartItem.create({
-      data: {
-        cartId: userCart.id,
-        productItemId: data.productItemId,
-        quantity: 1,
-        ingredients: {
-          connect: data.ingredients?.map((id) => ({ id })),
+    } else {
+      await prisma.cartItem.create({
+        data: {
+          cartId: userCart.id,
+          productItemId: data.productItemId,
+          quantity: 1,
+          ingredients: {
+            connect: data.ingredients?.map((id) => ({ id })),
+          },
         },
-      },
-    });
+      });
+    }
 
     const updatedUserCart = await updateCartTotalAmount(token);
     const resp = NextResponse.json(updatedUserCart);
