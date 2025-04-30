ad444fbcc8332d7dadf731eb4d43a6d7cfa3f73e
key-forr <denyskifor@gmail.com>
2025-04-30 21:56:08 +0300

    added validation for all the inputs

--- components/shared/checkout/checkout-personal-form.tsx
+++ components/shared/checkout/checkout-personal-form.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { WhiteBlock } from "../white-block";
-import { Input } from "@/components/ui";
 import { FormInput } from "../form";
 
 interface Props {
@@ -9,11 +8,15 @@ interface Props {
 
 export const CheckoutPersonalForm: React.FC<Props> = ({ className }) => {
   return (
-    <WhiteBlock title="2. Персональні дані">
+    <WhiteBlock title="2. Персональні дані" className={className}>
       <div className="grid grid-cols-2 gap-5">
-        <Input name="firstName" className="text-base" placeholder="Ім'я" />
-        <Input name="lastName" className="text-base" placeholder="Прізвище" />
-        <Input name="email" className="text-base" placeholder="E-Mail" />
+        <FormInput name="firstName" className="text-base" placeholder="Ім'я" />
+        <FormInput
+          name="lastName"
+          className="text-base"
+          placeholder="Прізвище"
+        />
+        <FormInput name="email" className="text-base" placeholder="E-Mail" />
         <FormInput name="phone" className="text-base" placeholder="Телефон" />
       </div>
     </WhiteBlock>
--- components/shared/form/form-input.tsx
+++ components/shared/form/form-input.tsx
@@ -28,6 +28,10 @@ export const FormInput: React.FC<Props> = ({
   const value = watch(name);
   const errorText = errors[name]?.message as string;
 
+  const onClickClear = () => {
+    setValue(name, "");
+  };
+
   return (
     <div className={className}>
       {label && (
@@ -39,7 +43,7 @@ export const FormInput: React.FC<Props> = ({
       <div className="relative">
         <Input className="h-12 text-md" {...register(name)} {...props} />
 
-        {value && <ClearButton />}
+        {value && <ClearButton onClick={onClickClear} />}
       </div>
 
       {errorText && <ErrorText text={errorText} className="mt-2" />}
