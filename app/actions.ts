e09bee5c16f0df896cdd8b02d913632a8fd67763
key-forr <denyskifor@gmail.com>
2025-05-03 17:12:34 +0300

    work with server actions

---
+++ app/actions.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { CheckoutFormValues } from "@/constants/checkout-form-schema";
+import { prisma } from "@/prisma/prisma-client";
+import { OrderStatus } from "@prisma/client";
+
+export async function createOrder(data: CheckoutFormValues) {
+  console.log(data);
+
+  const token = "123";
+
+  await prisma.order.create({
+    data: {
+      token,
+      totalAmount: 1500,
+      status: OrderStatus.PENDING,
+      items: [],
+      fullName: data.firstName + " " + data.lastName,
+      email: data.email,
+      phone: data.phone,
+      address: data.address,
+      comment: data.comment,
+    },
+  });
+}
