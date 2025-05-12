f039222c29ed5606cca4ba103df590f842b62e10
key-forr <denyskifor@gmail.com>
2025-05-12 22:29:22 +0300

    refactor code in file actions.ts

--- app/actions.ts
+++ app/actions.ts
@@ -3,25 +3,71 @@
 import { CheckoutFormValues } from "@/constants/checkout-form-schema";
 import { prisma } from "@/prisma/prisma-client";
 import { OrderStatus } from "@prisma/client";
+import { cookies } from "next/headers";
 
 export async function createOrder(data: CheckoutFormValues) {
-  console.log(data);
+  try {
+    const cookieStore = cookies();
+    const cartToken = (await cookieStore).get("cartToken")?.value;
 
-  const token = "123";
+    if (!cartToken) {
+      throw new Error("Cart token not found");
+    }
 
-  await prisma.order.create({
-    data: {
-      token,
-      totalAmount: 1500,
-      status: OrderStatus.PENDING,
-      items: [],
-      fullName: data.firstName + " " + data.lastName,
-      email: data.email,
-      phone: data.phone,
-      address: data.address,
-      comment: data.comment,
-    },
-  });
+    const userCart = await prisma.cart.findFirst({
+      include: {
+        user: true,
+        items: {
+          include: {
+            ingredients: true,
+            productItem: {
+              include: {
+                product: true,
+              },
+            },
+          },
+        },
+      },
+      where: {
+        token: cartToken,
+      },
+    });
 
-  return "https://github.com/key-forr/pizza/tree/order";
+    if (!userCart) {
+      throw new Error("Cart not found");
+    }
+
+    if (userCart?.totalAmount == 0) {
+      throw new Error("Cart is empty");
+    }
+
+    const order = await prisma.order.create({
+      data: {
+        token: cartToken,
+        fullName: data.firstName + " " + data.lastName,
+        email: data.email,
+        phone: data.phone,
+        address: data.address,
+        comment: data.comment,
+        totalAmount: userCart.totalAmount,
+        status: OrderStatus.PENDING,
+        items: JSON.stringify(userCart.items),
+      },
+    });
+
+    await prisma.cart.update({
+      where: {
+        id: userCart.id,
+      },
+      data: {
+        totalAmount: 0,
+      },
+    });
+
+    await prisma.cartItem.deleteMany({
+      where: {
+        cartId: userCart.id,
+      },
+    });
+  } catch (err) {}
 }
