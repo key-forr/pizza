c9c88f14a5c2adde7c42da3b5f115c9329887865
key-forr <denyskifor@gmail.com>
2025-05-22 18:46:43 +0300

    check user in server level

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,58 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "@/prisma/prisma-client";
+import { compare } from "bcrypt";
 
 export const authOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID || "",
       clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          return null;
+        }
+
+        const values = {
+          email: credentials.email,
+        };
+
+        const findUser = await prisma.user.findFirst({
+          where: values,
+        });
+
+        if (!findUser) {
+          return null;
+        }
+
+        const isPasswordValid = await compare(
+          credentials.password,
+          findUser.password
+        );
+
+        if (!isPasswordValid) {
+          return null;
+        }
+
+        if (!findUser.verified) {
+          return null;
+        }
+
+        return {
+          id: String(findUser.id),
+          email: findUser.email,
+          name: findUser.fullName,
+          role: findUser.role,
+        };
+      },
     }),
   ],
 };
