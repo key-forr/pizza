403cfc88f440faf8024e73b9abdc702215be92eb
key-forr <denyskifor@gmail.com>
2025-05-22 18:27:10 +0300

    login form already comp

--- components/shared/modals/auth-modal/forms/login-form.tsx
+++ components/shared/modals/auth-modal/forms/login-form.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { formLoginSchema, TFormLoginValues } from "./schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { FormInput, Title } from "@/components/shared";
+import { Button } from "@/components/ui";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 interface Props {
   onClose?: VoidFunction;
@@ -16,8 +20,28 @@ export const LoginFrom: React.FC<Props> = ({ onClose }) => {
     },
   });
 
-  const onSubmit = (data: TFormLoginValues) => {
-    console.log(data);
+  const onSubmit = async (data: TFormLoginValues) => {
+    try {
+      const resp = await signIn("credentials", {
+        ...data,
+        redirect: false,
+      });
+
+      if (!resp?.ok) {
+        throw Error();
+      }
+
+      toast.success("Ви успішно увійли в аккаунт", {
+        icon: "✅",
+      });
+
+      onClose?.();
+    } catch (error) {
+      console.error("Error [LOGIN]", error);
+      toast.error("Не вдалось увійти в аккаунт", {
+        icon: "❌",
+      });
+    }
   };
 
   return (
@@ -25,7 +49,33 @@ export const LoginFrom: React.FC<Props> = ({ onClose }) => {
       <form
         className="flex flex-col gap-5"
         onSubmit={form.handleSubmit(onSubmit)}
-      ></form>
+      >
+        <div className="flex justify-between items-center">
+          <div className="mr-2">
+            <Title text="Вхід в аккаунт" size="md" className="font-bold" />
+            <p className="text-gray-400">
+              Введіть свою пошту, щоб увійти в свій аккаунт
+            </p>
+          </div>
+          <img
+            src="/assets/images/phone-icon.png"
+            alt="phone-icon"
+            width={60}
+            height={60}
+          />
+        </div>
+
+        <FormInput name="email" label="E-Mail" required />
+        <FormInput name="password" label="Пароль" type="password" required />
+
+        <Button
+          loading={form.formState.isSubmitting}
+          className="h-12 text-base"
+          type="submit"
+        >
+          Увійти
+        </Button>
+      </form>
     </FormProvider>
   );
 };
