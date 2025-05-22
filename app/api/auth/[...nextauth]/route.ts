25d17c99efbf5a0969c289063da96202f810ebe4
key-forr <denyskifor@gmail.com>
2025-05-22 19:50:35 +0300

    github types for auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,12 +3,22 @@ import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "@/prisma/prisma-client";
 import { compare } from "bcrypt";
+import { UserRole } from "@prisma/client";
 
 export const authOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID || "",
       clientSecret: process.env.GITHUB_SECRET || "",
+      profile(profile) {
+        return {
+          id: profile.id,
+          name: profile.name || profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+          role: "USER" as UserRole,
+        };
+      },
     }),
     CredentialsProvider({
       name: "Credentials",
